3fdbcd4cd017025010d5b4ed46fc449041962053
Jeremy Wilford <jeremydwilford@gmail.com>
2022-09-15 18:19:01 -0700

    shizzle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PortfolioComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
